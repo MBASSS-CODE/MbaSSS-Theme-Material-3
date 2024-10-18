eb319cd36f726e848d26bf4b8e64092edae0026e
MbasssCode <mbassscode@gmail.com>
2024-10-18 13:03:15 +0700

    autocomplete use api (finish)

--- src/app/features/data/product/select-product/select-product.component.ts
+++ src/app/features/data/product/select-product/select-product.component.ts
@@ -4,7 +4,7 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DummyJSONService } from '../../../../services/api/dummy-json.service';
-import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, of, switchMap, tap } from 'rxjs';
 import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
@@ -20,43 +20,41 @@ import { MatProgressBar } from '@angular/material/progress-bar';
   ],
   template: `
     @if (this.loading) {
-      <mat-progress-bar color="primary"></mat-progress-bar>
+      <mat-progress-bar mode="indeterminate" color="primary"></mat-progress-bar>
     }
     <br>
-    <form class="example-form">
+    <form (submit)="onSubmit()">
       <mat-form-field class="example-full-width">
         <mat-label>Choose a product from the list</mat-label>
         <input type="text"
               placeholder="Pick one"
               aria-label="Search"
               matInput
-              [formControl]="formControl"
+              [formControl]="formControlAutocomplete"
               [matAutocomplete]="auto">
-        <mat-autocomplete #auto="matAutocomplete" (optionSelected)="onOptionSelected($event)">
+        <mat-autocomplete #auto="matAutocomplete"
+          (optionSelected)="onOptionSelected($event)"
+          [displayWith]="displayFn">
           @for (option of options; track option) {
-            <mat-option [value]="option.title">
+            <mat-option [value]="option">
               {{option.title}}
             </mat-option>
           }
         </mat-autocomplete>
       </mat-form-field>
+
+      <button mat-raised-button color="primary" type="submit" [disabled]="formControlAutocomplete.invalid">Submit</button>
     </form>
 
   `,
   styles: `
-    .example-form {
-      min-width: 150px;
-      max-width: 500px;
-      width: 100%;
-    }
-
     .example-full-width {
       width: 100%;
     }
   `
 })
 export default class SelectProductComponent {
-  formControl = new FormControl('');
+  formControlAutocomplete = new FormControl('');
   loading = false;
   optionSelected = false;
 
@@ -70,12 +68,14 @@ export default class SelectProductComponent {
       'title': 'Default show Product 2'
     },
   ];
-  
 
   constructor(
     private dummyJSONService: DummyJSONService
   ) {
-    this.formControl.valueChanges
+    this.initializeAutocomplete();
+  }
+  initializeAutocomplete(): void {
+    this.formControlAutocomplete.valueChanges
       .pipe(
         debounceTime(1000),
         switchMap((value) => {
@@ -83,22 +83,32 @@ export default class SelectProductComponent {
             this.optionSelected = false; // reset option selected flag
             return [];
           }
-          
           this.loading = true
-          return this.dummyJSONService.search(value)
+          return this.dummyJSONService.search(value).pipe(
+            catchError(() => { //if error, return empty array
+              this.loading = false;
+              return of([]);
+            })
+          )
         })
       ).subscribe(
-       (value: any) => {
+       (value: any) => { // on success
           this.options = value.products;
           this.loading = false;
-        },
-        () => {
-          this.loading = false; // Ensure loading is reset on error as well
         }
     )
   }
-
   onOptionSelected(event: MatAutocompleteSelectedEvent) {
-    this.optionSelected = true;
+    this.optionSelected = true; // set option selected flag to make sure we hit api only once
   }
+  displayFn(option: any) {
+    console.log('option', option);
+    return option && option.title ? option.title : '';
+  }
+
+  onSubmit() {
+    console.log(this.formControlAutocomplete.value);
+  }
+
+  
 }
